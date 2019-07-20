7c0c3916a2b54326b217f17129b74f14982a905f
madebyrh <madebyrh@gmail.com>
2019-07-21 00:58:56 +0900

    add product model

---
+++ lib/models/product.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/foundation.dart';
+
+class Product {
+  final String id;
+  final String title;
+  final String description;
+  final double price;
+  final String imageUrl;
+
+  // 変化するものはfinalにしない
+  bool isFavorite;
+
+  Product({
+    @required this.id,
+    @required this.title,
+    @required this.description,
+    @required this.price,
+    @required this.imageUrl,
+    this.isFavorite = false,
+  });
+}
