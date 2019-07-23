090f228586342d6177a8a148744cee0484b38446
madebyrh <madebyrh@gmail.com>
2019-07-23 23:16:30 +0900

    add text input

---
+++ lib/screens/edit_product_screen.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+
+// submitするまでは、このウィジェット内で値の変更が見えるようにするため(local state)
+// StateFulを使用する
+class EditProductScreen extends StatefulWidget {
+  static const routeName = '/edit-product';
+  @override
+  _EditProductScreenState createState() => _EditProductScreenState();
+}
+
+class _EditProductScreenState extends State<EditProductScreen> {
+  // titleのソフトウェアキーボードでenterを押すとpriceのinputに移動する
+  final _priceFocusNode = FocusNode();
+  final _descriptionFocusNode = FocusNode();
+
+  // FocusNodeはメモリを使うので、メモリリークを防ぐため
+  @override
+  void dispose() {
+    _priceFocusNode.dispose();
+    _descriptionFocusNode.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Edit Product'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Form(
+          child: ListView(
+            children: <Widget>[
+              // Formの中ではTextFieldではなく、TextFormField
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Title'),
+                textInputAction: TextInputAction.next,
+                onFieldSubmitted: (_) {
+                  FocusScope.of(context).requestFocus(_priceFocusNode);
+                },
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Price'),
+                textInputAction: TextInputAction.next,
+                keyboardType: TextInputType.number,
+                focusNode: _priceFocusNode,
+                onFieldSubmitted: (_) {
+                  FocusScope.of(context).requestFocus(_descriptionFocusNode);
+                },
+              ),
+              TextFormField(
+                decoration: InputDecoration(labelText: 'Description'),
+                maxLines: 3,
+                keyboardType: TextInputType.multiline,
+                focusNode: _descriptionFocusNode,
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
